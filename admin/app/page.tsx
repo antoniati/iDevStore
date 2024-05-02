f30548437d1032e042f39d50268ce225e83ffc49
antoniati <antoniati.felipe@gmail.com>
2024-05-02 16:27:56 -0300

    feat: Initial commit, add Next.js 14, Typescript, Tailwind CSS, eslint, Tremor Components, Logo, FavIcon, and config files tailwind e typescript. Hello World

---
+++ admin/app/page.tsx
@@ -0,0 +1,13 @@
+import { Card, Flex, Title } from "@tremor/react";
+
+export default function Home() {
+  return (
+    <Flex>
+      <Card>
+        <Title>
+          Hello World
+        </Title>
+      </Card>
+    </Flex>
+  );
+};
